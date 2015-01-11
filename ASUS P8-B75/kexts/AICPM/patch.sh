66fddf46efff028691d5f1d2582de46ad0ec62e9
mateass <kukri.mate@gmail.com>
2015-01-11 22:25:16 +0100

    Added ASUS P8-B75V

---
+++ ASUS P8-B75/kexts/AICPM/patch.sh
@@ -0,0 +1,3 @@
+cd AICPMPatch
+sudo perl AICPMPatch.pl /System/Library/Extensions/AppleIntelCPUPowerManagement.kext/Contents/MacOS/AppleIntelCPUPowerManagement --patch
+sudo touch /System/Library/Extensions
